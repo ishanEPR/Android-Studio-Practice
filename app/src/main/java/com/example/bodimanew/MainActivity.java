b9c71bc420fb92d8be0f0207c08e12d2fd3a9977
ishan <reshmikaediriweera1997@gmail.com>
2020-12-19 12:14:23 +0530

    My first project

---
+++ app/src/main/java/com/example/bodimanew/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bodimanew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
